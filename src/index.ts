76bbf0d66dd1cc7d9e5250623155eb052e146c06
Dup4 <lyuzhi.pan@gmail.com>
2022-03-13 23:21:50 +0800

    chore: lint code style format

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,60 @@
-import { Node, Root } from 'hast';
-import { h } from 'hastscript';
-import { Plugin } from 'unified';
-import { visit } from 'unist-util-visit';
+import type { Node } from 'hast'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Root } from 'hast'
+import { h } from 'hastscript'
+import type { Plugin } from 'unified'
+import { visit } from 'unist-util-visit'
 
-import { fromMarkdownDetails } from './mdast-util-details/index.js';
-import { syntax } from './micromark-details/index.js';
+import { fromMarkdownDetails } from './mdast-util-details/index.js'
+import { syntax } from './micromark-details/index.js'
 
 interface DetailsNode extends Node {
-	name: string;
-	attributes: { open: boolean; class?: string };
+  name: string
+  attributes: {
+    open: boolean
+    class?: string
+  }
 }
 
-let warningIssued = false;
-const remarkDetails: Plugin = function (options) {
-	const data = this.data() as Record<string, unknown[] | undefined>;
-	// warning for old remarks
-	if (
-		!warningIssued &&
-		(this.Parser?.prototype?.blockTokenizers ||
-			this.Compiler?.prototype?.visitors)
-	) {
-		warningIssued = true;
-		console.warn(
-			'[remark-details] Warning: please upgrade to remark 13 to use this plugin',
-		);
-	}
+let warningIssued = false
 
-	function add(field: string, value: unknown) {
-		/* istanbul ignore if - other extensions. */
-		if (data[field]) data[field]!.push(value);
-		else data[field] = [value];
-	}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const remarkDetails: Plugin = function(options) {
+  const data = this.data() as Record<string, unknown[] | undefined>
+  // warning for old remarks
+  if (
+    !warningIssued
+    && (
+      this.Parser?.prototype?.blockTokenizers
+    || this.Compiler?.prototype?.visitors)
+  ) {
+    warningIssued = true
+    console.warn(
+      '[remark-details] Warning: please upgrade to remark 13 to use this plugin',
+    )
+  }
 
-	add('micromarkExtensions', syntax);
-	add('fromMarkdownExtensions', fromMarkdownDetails);
+  function add(field: string, value: unknown) {
+    /* istanbul ignore if - other extensions. */
+    if (data[field]) data[field]!.push(value)
+    else data[field] = [value]
+  }
 
-	return transform;
+  add('micromarkExtensions', syntax)
+  add('fromMarkdownExtensions', fromMarkdownDetails)
 
-	function transform(tree) {
-		visit(tree, ['detailsContainer', 'detailsContainerSummary'], ondetails);
-	}
+  return transform
 
-	function ondetails(node: DetailsNode) {
-		var data = node.data || (node.data = {});
-		var hast = h(node.name, node.attributes);
+  function transform(tree) {
+    visit(tree, ['detailsContainer', 'detailsContainerSummary'], ondetails)
+  }
 
-		data.hName = hast.tagName;
-		data.hProperties = hast.properties;
-	}
-};
-export default remarkDetails;
+  function ondetails(node: DetailsNode) {
+    const data = node.data || (node.data = {})
+    const hast = h(node.name, node.attributes)
+
+    data.hName = hast.tagName
+    data.hProperties = hast.properties
+  }
+}
+export default remarkDetails
--- test/micromark-details.test.ts
+++ test/micromark-details.test.ts
@@ -1,30 +1,34 @@
-import testCase from "./test-case.js";
-import { micromark } from 'micromark';
-import { syntax, detailsHtml as html, } from '../lib/micromark-details/index.js';
+import { micromark } from 'micromark'
+import { detailsHtml as html, syntax } from '../lib/micromark-details/index.js'
+import testCase from './test-case.js'
 
 testCase.processor = (input: string) => {
-	return micromark(input, {
-		extensions: [syntax],
-		htmlExtensions: [html as any]
-	});
+  return micromark(input, {
+    extensions: [syntax],
+    htmlExtensions: [html as any],
+  })
 }
+
 testCase({
-	input: 'hello world',
-	expected: '<p>hello world</p>',
-	message: 'basic test',
-});
+  input: 'hello world',
+  expected: '<p>hello world</p>',
+  message: 'basic test',
+})
+
 testCase({
-	input: '???+总结如下\n',
-	expected: '<details open><summary>总结如下</summary>\n</details>',
-	message: 'no content details',
-});
+  input: '???+总结如下\n',
+  expected: '<details open><summary>总结如下</summary>\n</details>',
+  message: 'no content details',
+})
+
 testCase({
-	input: '???+ note 总结\n',
-	expected: '<details open class="note"><summary>总结</summary>\n</details>',
-	message: 'no content details with note',
-});
+  input: '???+ note 总结\n',
+  expected: '<details open class="note"><summary>总结</summary>\n</details>',
+  message: 'no content details with note',
+})
+
 testCase({
-	input: '???+ warning 总结\n',
-	expected: '<details open class="warning"><summary>总结</summary>\n</details>',
-	message: 'no content details with warning',
-});
+  input: '???+ warning 总结\n',
+  expected: '<details open class="warning"><summary>总结</summary>\n</details>',
+  message: 'no content details with warning',
+})
